962509e7993e3156f2ec3c3e602750402def5bcc
suryajude <jsurya1230@gmail.com>
2023-09-20 22:10:56 -0700

    numbers and functions

---
+++ fullstack/javascript/numbers.js
@@ -0,0 +1,19 @@
+// JESUS is the true living GOD
+
+var age = 70;
+var pointednumber = 34.6789;
+var ageInString = "42";
+
+
+console.log(age.toString()); // convert the int value to string
+console.log(pointednumber.toExponential(2)); // it will give the exponential value
+console.log(ageInString.valueOf()); // convert the type int to string
+console.log(pointednumber.toFixed(3)); // it will give the fixed value after the point
+console.log(pointednumber.toPrecision(3)); // it will give total 3 digits or the number of digits which we parse 
+// inside the parameter
+console.log(Number(ageInString));  // to convert a string into a number 
+console.log(parseInt(ageInString)) // to convert a string into a number
+
+
+console.log(typeof ageInString); //used to check the data type
+console.log(typeof pointednumber); 
--- fullstack/javascript/script.js
+++ fullstack/javascript/script.js
@@ -6,6 +6,7 @@ var count = 0;
 var time = 0;
 button.addEventListener("click",function(){
     count+=1;
+    title.innerText = new Date();
     if(count==1){
         setInterval(function(){
             time = time+1;
@@ -22,3 +23,15 @@ button.addEventListener("click",function(){
         null;
     }
 });
+
+console.log("it is a log"); // to print a value
+console.warn("It is a warning"); // to print a value witha  yellow theme
+console.error("It is an error"); // to print a value with red theme
+
+/**
+ * IT is used for programming
+ * to handle our views
+ * ----> for controlling our views
+ * ----> controlling forms, para, buttons,etc...
+ * ----> DOM = Document Object Model.
+ */
