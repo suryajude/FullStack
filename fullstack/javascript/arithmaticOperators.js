62cd4ddf446975f48b48c657948352178f69b18f
suryajude <jsurya1230@gmail.com>
2023-09-20 22:40:59 -0700

    arithmatic operators

---
+++ fullstack/javascript/arithmaticOperators.js
@@ -0,0 +1,14 @@
+// Here we use the arithmatic operators
+
+var english = 50;
+var maths = 49;
+var science = 44;
+var arts = 46;
+
+var total = 100;
+
+console.log(english+maths+science+arts);
+console.log(total-english);
+console.log(total/2);
+console.log(total *5);
+console.log(total%2);
